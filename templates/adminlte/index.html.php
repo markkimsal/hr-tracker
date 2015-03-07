2f85021688859f026e66516fb6eebf39ab6ff7f6
Mark Kimsal <metrofindings@gmail.com>
2015-03-07 12:16:12 -0500

    Customize x-editable css for 100% widths inline.
    Not 100% widths looks really bad on inline forms.

--- templates/adminlte/index.html.php
+++ templates/adminlte/index.html.php
@@ -8,8 +8,6 @@
         <link href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
         <!-- Theme style -->
         <link href="<?php echo m_turl();?>css/adminlte.css" rel="stylesheet" type="text/css" />
-        <!-- inline editing -->
-        <link href="//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/bootstrap3-editable/css/bootstrap-editable.css" rel="stylesheet"/>
 
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
